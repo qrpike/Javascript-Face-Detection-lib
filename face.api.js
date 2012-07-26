be267211ced6dfad3def95520b6dd1ca8d4c605a
Quinton Pike <qrpike@gmail.com>
2012-07-26 18:16:29 -0300

    Update face.api.js
    removed instructions as they are on GitHub

--- face.api.js
+++ face.api.js
@@ -1,31 +1,9 @@
-
 /* 
 
 	Development by Quinton Pike
 	contact: qrpike@gmail.com
 	
-	Use of Object:
-	
-	Must include before this file: (These were not written by me)
-	ccv.js
-	face.js
-	
-	-- HTML --
-	
-	<canvas id="output"></canvas>
-	
-	
-	
-	-- Javascript -- 
-	
-	var detector = new faceDetect({ imageUrl:'/path/to/local/image' , canvasID:'output' });
-	detector.detectFace(function(){
-		console.log('Done detecting image');
-		// detector.timeElapsed : Time it took to detect faces..
-		// detector.numFaces : Number of faces found..
-		// detector.pos : Position of the faces..
-	});
-	
+	GitHub: https://github.com/qrpike/Javascript-Face-Detection-lib/ <-- for instructions on using this lib.
 
 */
 
